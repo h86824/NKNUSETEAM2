b1c0577bf22ca4234a891ff63de3a34653771b46
tommy1681 <tommy1681@gmail.com>
2016-12-06 00:21:39 +0800

    1
    123

--- src/EssObject/Coach.java
+++ src/EssObject/Coach.java
@@ -3,6 +3,6 @@ package EssObject;
 public class Coach {
     public Coach(String name, String gender, int age, String profession) {
         super(name, gender, age, profession);
-
+//123123
       }
 }
