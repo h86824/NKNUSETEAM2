b37e0c47dea31b37aa6b4e1a574a430e795b4ba6
tommy1681 <tommy1681@gmail.com>
2016-12-13 19:48:11 +0800

    修改Event、賽事記錄

--- src/EssObject/MedicalHistory.java
+++ src/EssObject/MedicalHistory.java
@@ -1,5 +1,31 @@
 package EssObject;
 
+import java.util.ArrayList;
+
+import java.util.Collections;
+
 public class MedicalHistory {
 	
+	String name;
+	ArrayList<String> medicalHistory=new ArrayList<String>();
+	public MedicalHistory(String name){
+		this.name = name;
+	}
+	public void addMedicalHistory(String medical){
+		medicalHistory.add(medical);
+	}
+	public ArrayList<String> getMedicalHistory(){
+		Collections.sort(medicalHistory);
+		return medicalHistory;
+	}
+	public boolean equals(Object o){
+		if(this.getClass() != o.getClass())return false;
+		MedicalHistory medicalHistory=(MedicalHistory) o;
+		if(!this.name.equals(medicalHistory.name)){
+			return false;
+		}
+		else{
+			return true;
+		}
+	}
 }
