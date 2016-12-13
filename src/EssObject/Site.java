22269c4b46989f1010a4c62d1033efd95d8bcecd
tommy1681 <tommy1681@gmail.com>
2016-12-13 22:27:07 +0800

    修改site

--- src/EssObject/Site.java
+++ src/EssObject/Site.java
@@ -15,10 +15,13 @@ public class Site {
 		return "site name = "+name;
 	}
 	public boolean equals(Object o){
-		if(o.getClass() != this.getClass())return false;
+		if(o.getClass() != this.getClass()){
+			return false;
+		}
 		Site site = (Site)o;
-		if(site.name != this.name)return false;
-		return true;
+		if(site.name != this.name)return false;{
+			return true;
+		}
 	}
 	public int compareTo(Site o) {
 		return this.name.compareTo(o.name);
