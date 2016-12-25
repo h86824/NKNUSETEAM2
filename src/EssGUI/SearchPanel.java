8a73d674d176565f93d9de21bb60f76efdc326d3
tommy1681 <tommy1681@gmail.com>
2016-12-26 02:18:36 +0800

    查詢增加選單與選單事件

--- src/EssGUI/SearchPanel.java
+++ src/EssGUI/SearchPanel.java
@@ -1,24 +1,35 @@
 package EssGUI;
 
+import java.awt.Container;
+import java.awt.Font;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
+import java.awt.GridLayout;
+import java.awt.Label;
+import java.awt.SystemColor;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.ItemEvent;
+import java.awt.event.WindowAdapter;
 
 import javax.swing.JButton;
+import javax.swing.JComboBox;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JTextArea;
 import javax.swing.JTextField;
 
 import EssIO.DataStore;
+import EssObject.Country;
 
 
-public class SearchPanel extends JPanel{
+public class SearchPanel extends JPanel implements ActionListener{
 	JLabel jLabel = new JLabel();
-	
-	
+	JComboBox<String> box= new JComboBox<String>(); 
+	JTextField field = new JTextField();
 	DataStore dataStore;
+	
 	
 	
 	
@@ -27,6 +38,7 @@ public class SearchPanel extends JPanel{
 		GridBagLayout GBL = new GridBagLayout();
 		this.setLayout(GBL);
 		GridBagConstraints G=new GridBagConstraints();
+		
 		G.gridx = 0;
         G.gridy = 0;
         G.gridwidth = 6;
@@ -35,11 +47,13 @@ public class SearchPanel extends JPanel{
         G.weighty = 0;
         G.fill = GridBagConstraints.BOTH;
         G.anchor = GridBagConstraints.WEST;
-        
         this.add(field);
         GBL.setConstraints(field, G);
-        JButton jb = new JButton("查詢");
+        field.setText("選手A");
         
+        
+        JButton jb = new JButton("查詢");
+        jb.addActionListener(this);
 		G.gridx = 10;
         G.gridy = 0;
         G.gridwidth = 1;
@@ -50,24 +64,69 @@ public class SearchPanel extends JPanel{
         G.anchor = GridBagConstraints.WEST;
 		this.add(jb);
 		GBL.setConstraints(jb, G);
-		field.setText("請輸入國家名");
 		
 		
-		this.add(jLabel);
+		
+		
+		String[] countryList = new String[dataStore.getCountry().size()];
+		int count = 0;
+		for(Country i : dataStore.getCountry()){
+			countryList[count] = i.getName();
+			count++;
+		}
+		for(int i=0 ; i<countryList.length ;i++){
+			box.addItem(countryList[i]);
+		}
+		this.add(box);
+		
+		
 	}
-	JTextField field = new JTextField();
+	
+		
+ 
+
+
+	@Override
+	
+	public void actionPerformed(ActionEvent arg0) {
+		String returnStr[] = new String[2];
+		box.getSelectedItem().toString(); 
+		returnStr[0]=box.getSelectedItem().toString();
+		returnStr[1]=field.getText();
+		new massage(returnStr);
+		System.out.println(returnStr[1]);
+	}
 	
 	
-}	
-
-class ButtonHandler implements ActionListener{
-
-	 private DataStore dataStore;
-	public void actionPerformed(ActionEvent Event){ 
-		} 
-	 	JPanel panel = new SearchPanel(dataStore);
-	 		
-		} 
+}
+class massage extends JFrame{
+	JLabel jLabel = new JLabel();
+	JTextArea jTextArea1 = new JTextArea();
+	JPanel Pan=new JPanel(null); 
+	public massage(String [] a){
+		JButton jb = new JButton("查詢");
+		Container c = getContentPane(); 
+		c = this.getContentPane(); 
+		this.setTitle("查詢結果");
+		c.setLayout(new GridLayout(3, 1));
+		setSize(400,300);
+		setLocation(200,120); 
+		setVisible(true);
+		
+		
+		jTextArea1.setBackground(SystemColor.control);
+		jTextArea1.setText("國家:"+a[0]+"\r\n"+"名字:"+a[1]);
+		c.add(jTextArea1);
+		
+		
+		
+	}
+	
+	
+	
+	
+	
+}
 
 
 
