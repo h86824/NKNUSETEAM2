13b5c590e239835285169e8d390b33118865d19c
tommy1681 <tommy1681@gmail.com>
2016-12-26 16:58:43 +0800

    查詢介面更新

--- src/EssGUI/SearchPanel.java
+++ src/EssGUI/SearchPanel.java
@@ -5,6 +5,7 @@ import java.awt.Font;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
 import java.awt.GridLayout;
+import java.awt.Insets;
 import java.awt.Label;
 import java.awt.SystemColor;
 import java.awt.event.ActionEvent;
@@ -16,6 +17,7 @@ import javax.swing.JButton;
 import javax.swing.JComboBox;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JList;
 import javax.swing.JPanel;
 import javax.swing.JTextArea;
 import javax.swing.JTextField;
@@ -26,48 +28,48 @@ import EssObject.Country;
 
 public class SearchPanel extends JPanel implements ActionListener{
 	JLabel jLabel = new JLabel();
+	JLabel jLabel1 = new JLabel();
 	JComboBox<String> box= new JComboBox<String>(); 
 	JTextField field = new JTextField();
 	DataStore dataStore;
-	
+	JTextArea jTextArea2= new JTextArea();
 	
 	
 	
 	public SearchPanel(DataStore dataStore){
 		this.dataStore=dataStore;
 		GridBagLayout GBL = new GridBagLayout();
+		this.setBackground(SystemColor.GRAY);
 		this.setLayout(GBL);
-		GridBagConstraints G=new GridBagConstraints();
 		
-		G.gridx = 0;
-        G.gridy = 0;
-        G.gridwidth = 6;
+		
+		GridBagConstraints G=new GridBagConstraints();
+		G.gridwidth = 2;
         G.gridheight = 1;
-        G.weightx = 0;
-        G.weighty = 0;
-        G.fill = GridBagConstraints.BOTH;
-        G.anchor = GridBagConstraints.WEST;
+		G.anchor = GridBagConstraints.WEST;
+		G.fill = GridBagConstraints.NONE;
+		jLabel1.setText("請輸入選手名:");
+		this.add(jLabel1,G);
+		
+		
+		
+		
+		
+		
+		
+		
+		G.weightx=1;
+		G.weighty=1;
+        G.gridwidth = 3;
+        G.gridheight = 1;
+        G.fill = GridBagConstraints.HORIZONTAL;
+        G.anchor = GridBagConstraints.EAST;
         this.add(field);
         GBL.setConstraints(field, G);
         field.setText("選手A");
         
         
-        JButton jb = new JButton("查詢");
-        jb.addActionListener(this);
-		G.gridx = 10;
-        G.gridy = 0;
-        G.gridwidth = 1;
-        G.gridheight = 1;
-        G.weightx = 0;
-        G.weighty = 0;
-        G.fill = GridBagConstraints.BOTH;
-        G.anchor = GridBagConstraints.WEST;
-		this.add(jb);
-		GBL.setConstraints(jb, G);
-		
-		
-		
-		
+       
 		String[] countryList = new String[dataStore.getCountry().size()];
 		int count = 0;
 		for(Country i : dataStore.getCountry()){
@@ -77,8 +79,49 @@ public class SearchPanel extends JPanel implements ActionListener{
 		for(int i=0 ; i<countryList.length ;i++){
 			box.addItem(countryList[i]);
 		}
-		this.add(box);
+		G.gridwidth = 3;
+        G.gridheight = 1;
+        G.ipadx = 3;
+        G.fill = GridBagConstraints.NONE;
+        G.anchor = GridBagConstraints.CENTER;
+		this.add(box,G);
 		
+		
+		
+        JButton jb = new JButton("查詢");
+        jb.addActionListener(this);
+        G.gridwidth = 0;
+        G.gridheight = 1;
+        G.fill = GridBagConstraints.NONE;
+        G.anchor = GridBagConstraints.EAST;
+		this.add(jb);
+		GBL.setConstraints(jb, G);
+		
+		 
+		
+		
+		String[] city={"北京                                                             ","上海","長沙","深圳","南京"};
+		JList<String> jl =new JList<String>(city);
+		G.insets=new Insets(0,10,10,10);
+		G.gridwidth = 4;
+        G.gridheight = 10;
+        G.weightx=1;
+        G.weighty=10;
+        G.fill = GridBagConstraints.BOTH;
+        G.anchor = GridBagConstraints.NORTHWEST;
+		this.add(jl,G);
+		
+		
+		G.insets=new Insets(0,10,10,10);
+		G.gridwidth = 6;
+		/*G.gridwidth =2;
+        G.gridheight = 10;
+        G.weightx=1;
+        G.weighty=10;*/
+        G.fill = GridBagConstraints.BOTH;
+        G.anchor = GridBagConstraints.WEST;
+        this.add(jTextArea2,G);
+        jTextArea2.setBackground(SystemColor.WHITE);
 		
 	}
 	
@@ -94,6 +137,8 @@ public class SearchPanel extends JPanel implements ActionListener{
 		returnStr[0]=box.getSelectedItem().toString();
 		returnStr[1]=field.getText();
 		new massage(returnStr);
+		
+		jTextArea2.setText("國家:"+"\r\n"+"名字:");
 		System.out.println(returnStr[1]);
 	}
 	
@@ -108,7 +153,7 @@ class massage extends JFrame{
 		Container c = getContentPane(); 
 		c = this.getContentPane(); 
 		this.setTitle("查詢結果");
-		c.setLayout(new GridLayout(3, 1));
+		c.setLayout(new GridLayout(1, 1));
 		setSize(400,300);
 		setLocation(200,120); 
 		setVisible(true);
